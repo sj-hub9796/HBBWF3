e6d8377f3335993ed5ef36a351bd83ff07d072fa
sjhub <sj.hub9796@gmail.com>
2025-05-29 20:47:15 +0900

    support fabric 1.20.1

--- src/main/java/net/sjhub/hbbwf3/mixin/BossBarRenderMixin.java
+++ src/main/java/net/sjhub/hbbwf3/mixin/BossBarRenderMixin.java
@@ -3,6 +3,7 @@ package net.sjhub.hbbwf3.mixin;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.BossBarHud;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,10 +13,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(BossBarHud.class)
 public class BossBarRenderMixin {
 
-    @Shadow private MinecraftClient client;
+    @Shadow @Final private MinecraftClient client;
 
     @Inject(method = "render", at = @At("HEAD"), cancellable = true)
     private void render(DrawContext context, CallbackInfo ci) {
-        if (client.getDebugHud().shouldShowDebugHud()) ci.cancel();
+        if (client.options.debugEnabled) ci.cancel();
     }
 }
